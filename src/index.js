60dc4d1b68eba7db101d19a54421b37731ffbe07
AlokHimself <alokhiself@gmail.com>
2021-07-20 20:52:21 +0100

    lab work

---
+++ src/index.js
@@ -0,0 +1,107 @@
+"use strict";
+
+/**
+ Problem #1: getElementById('id') practice
+
+  Using the following HTML mark up, your goal is to write code in index.js to select the following elements using getElementById.
+  1. Select the image element by its id and save it to a variable called image
+  2. Select the h1 by its id and save it to a varible called heading
+
+         <!DOCTYPE html>
+        <html>
+
+        <head>
+            <title>Unicorn</title>
+        </head>
+
+        <body>
+            <!--DON'T TOUCH THIS PLEASE!-->
+            <h1 id="mainheading">I &hearts; Unicorns</h1>
+            <img id="unicorn" src="https://www.flaticon.com/svg/static/icons/svg/3468/3468081.svg" alt="" width="200px">
+            <!-- Icon made by https://www.flaticon.com/authors/freepik  -->
+
+        </body>
+
+        </html>
+
+
+ */
+
+// ANSWER
+
+
+/**
+ Problem #2: querySelector Practice
+
+  Using the following HTML mark up and CSS, your goal is to write code in index.js to select the following elements using querySelector and querySelectorAll.
+    1. Select all the elements tha have the class of 'done' and save them in a varible called 'doneTodos'
+    2. Select the 'one' checkbox and save it in a varible called 'checkbox'. Be careful, more than one input element in the page.
+
+   Please do not change any element of this HTML markup
+
+       *<!DOCTYPE html>
+      <html>
+
+      <head>
+          <title>Todos</title>
+      </head>
+
+      <body>
+          <h1>Garden Todos</h1>
+          <input type="text" placeholder="New Todo">
+          <ul>
+              <li>Start Seedlings</li>
+              <li class="done">Deadhead Zinnias</li>
+              <li class="done">Water Tomatoes</li>
+              <li class="done">Harvest Potatoes</li>
+              <li>Prune Roses</li>
+          </ul>
+          <label>Delete All</label>
+          <input type="checkbox" id="scales" name="scales" checked>
+
+      </body>
+
+      </html>
+
+
+
+  Please do not make any changes to the CSS below
+
+  .done {
+      color: grey;
+      text-decoration: line-through;
+        }
+
+
+ */
+
+// ANSWER
+
+
+/**
+  Problem #3: Change text using JavaScript
+
+   Using the following HTML mark up, your goal is to select '<span>' element using JavaScript and change its text to read 'Disgusting'.
+   Don't cheat by using HTML file directly.
+
+   Please do not change any element of this HTML markup
+   
+      <!DOCTYPE html>
+
+            <head>
+                <title>Pickles</title>
+
+            </head>
+
+            <body>
+                <!--PLEASE LEAVE THIS LINE ALONE! MAKE YOUR CHANGES USING JAVASCRIPT!!-->
+                <h1>Pickles Are <span>Delicious</span></h1>
+            </body>
+
+            </html>
+
+
+
+ */
+
+// ANSWER
